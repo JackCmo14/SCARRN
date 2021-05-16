c4f7445739d0202605a3ecfdfbfc19fa9aeb3201
Jack Morris <jackCmo14@outlook.com>
2021-05-16 15:24:02 +0100

    Add files via upload

--- Application Data Files/Secure Code Files/RALogin1S.php
+++ Application Data Files/Secure Code Files/RALogin1S.php
@@ -5,36 +5,43 @@ global $password;
 
 include "includes/connection.php";
 
-//$msg = "";
-$form_user = $_POST['txtusername'];
-$form_password = $_POST['txtpassword'];
-
-	if($form_user  == "" || $form_password == "")
-	{
-		echo '<script>alert("Please Fill up all fields!");</script>';
-		echo "<script>window.location.href='default.php';</script>";	
-		exit;
+//Function to sanitize values received from the form. Prevents SQL injection
+function clean($str) {
+	$str = @trim($str);
+	if(get_magic_quotes_gpc()) {
+		$str = stripslashes($str);
 	}
-	
-$username = $_POST['txtusername'];
-$password = $_POST['txtpassword'];
-
-
-	//Function to sanitize values received from the form. Prevents SQL injection
-	function clean($str) {
-		$str = @trim($str);
-		if(get_magic_quotes_gpc()) {
-			$str = stripslashes($str);
-		}
-		return mysql_real_escape_string($str);
-	}
+	return mysql_real_escape_string($str);
+}
 
 include 'sqlcm_filter.php';
+
+if($stmt = $con->prepare("select * from users WHERE txtusername=?")) {
+  $stmt->bind_param('s', $_POST['txtusername']);
+  $stmt->execute();
+  $stmt->store_result();
+  if($stmt->num_rows>0){
+    $stmt->bind_result($id, $password);
+    $stmt->fetch();
+    if(password_verify($_POST['txtpassword'],$password)){
+      session_regenerate_id();
+      $_SESSION['txtusername']=$rows['txtusername'];
+      echo "<script>alert('You're successfully logged in!')</script>";
+    	echo "<script>window.open('customers/index.php','_self')</script>";
+    }
+    else{
+      echo "<script>alert('Email or password is incorrect!')</script>";
+    echo "<script>window.open('index.php','_self')</script>";
+    }
+  }
+  $stmt->close();
+}
 
 	//Store values in the tblChatUsers
 	$dbconnect = new DbConnect();
 	$dbconnect->open();
-	$sql = "SELECT user_id, username, password, ac_type, user_status, thumbnail FROM `users` WHERE username = '$username';";
+	$sql = "SELECT * FROM `users` WHERE username = '?'";
+	$sql->bind_param('s', $_POST['username'])
 	$numrecs = 0;
 	$dbquery = new DbQuery($sql);
 	$result = $dbquery->query();
@@ -42,9 +49,9 @@ include 'sqlcm_filter.php';
 	$dbquery->freeresult();
 	$dbquery->close();
 
-echo $username; 
+echo $username;
 
-//SQL countermeasure. 
+//SQL countermeasure.
 include 'sqlcm.php';
 include 'username.php';
 include 'cookie.php';
@@ -52,7 +59,8 @@ include 'cookie.php';
 	//Store values in the tblChatUsers
 	$dbconnect = new DbConnect();
 	$dbconnect->open();
-	$sql = "SELECT user_id, username, password, ac_type, user_status, thumbnail FROM `users` WHERE username = '$username' AND password = '$password';";
+	$sql = "SELECT user_id, username, password, ac_type, user_status, thumbnail FROM `users` WHERE username = '?' AND password = '?'";
+	$sql->bind_param('s', $_POST['username'])
 	$numrecs = 0;
 //	$username=$form_user;
 	$dbquery = new DbQuery($sql);
@@ -73,11 +81,11 @@ include 'cookie.php';
 		if(($status == "0") AND ($ac_type == "Administrator"))
 		{
 			$_SESSION['status'] = "admin";
-			
+
 			echo "<script>alert('Welcome Back Webmaster Redirecting to personal home page')</script>";
 			echo "<script>window.location.href='adminarea/adminhome.php';</script>";
 		}
-		
+
 		else if(($status == "1") AND ($ac_type == "user"))
 		{
 			$user_id = $row[0];
@@ -85,17 +93,17 @@ include 'cookie.php';
 			echo "<script>alert('Welcome Back')</script>";
 			echo "<script>window.location.href='index.php';</script>";
 		}
-		
+
 		else
 		{
 			echo "<script>window.location.href='index.php';</script>";
 		}
 	}
-	
+
 	$dbquery->freeresult();
 	$dbquery->close();
-	
-	
+
+
 	if($numrecs==0)
 	{
 		echo '<script>alert("Password not found! \n\n");</script>';
@@ -114,12 +122,12 @@ include 'cookie.php';
 		//$status = $_SESSION['status'];
 		$_SESSION['thumbnail'] = $thumbnail;
 		$_SESSION['code'] = rand();
-	
+
 		//echo "<script>parent.reloadUsers();</script>";
 		echo "<script>window.location.href='index.php';</script>";
 		//exit;
 	}
-		
+
 	//instantiate object of query class
 	$dbquery = new DbQuery($sql);
 	$dbquery->query();
