c4290a8c50c532597c695fcaec6ba76258dd036d
Jack Morris <jackCmo14@outlook.com>
2021-05-16 14:55:33 +0100

    Add files via upload

--- Application Data Files/Secure Code Files/AstleyBoards1S.php
+++ Application Data Files/Secure Code Files/AstleyBoards1S.php
@@ -12,31 +12,32 @@ if(isset($_POST['user_login']))
 $con=mysql_connect("localhost","root","Hacklab1") or die ("DOWN!");
 mysql_select_db("edgedata",$con);
 
-//include 'sqlcm_filter.php';
-
-    $sql=mysql_query("select * from users WHERE user_email='$username'");
-    $rows= mysql_fetch_array($sql);
-
+include 'sqlcm_filter.php';
 include 'sqlcm.php';
 include 'username.php';
 include 'cookie.php';
 
-    $sql=mysql_query("select * from users WHERE user_email='$username' AND user_password='$password'");
-    $rows=mysql_fetch_array($sql);
-
-if($rows>0)
-    {
-	echo "<script>alert('You're successfully logged in!')</script>";
-	echo "<script>window.open('customers/index.php','_self')</script>";
-	$_SESSION['user_email']=$rows['user_email'];
+if($stmt = $con->prepare("select * from users WHERE user_email=?")) {
+  $stmt->bind_param('s', $_POST['user_email']);
+  $stmt->execute();
+  $stmt->store_result();
+  if($stmt->num_rows>0){
+    $stmt->bind_result($id, $password);
+    $stmt->fetch();
+    if(password_verify($_POST['user_password'],$password)){
+      session_regenerate_id();
+      $_SESSION['user_email']=$rows['user_email'];
+      echo "<script>alert('You're successfully logged in!')</script>";
+    	echo "<script>window.open('customers/index.php','_self')</script>";
     }
-    else
-    {
-        echo "<script>alert('Email or password is incorrect!')</script>";
-		  echo "<script>window.open('index.php','_self')</script>";
-
+    else{
+      echo "<script>alert('Email or password is incorrect!')</script>";
+    echo "<script>window.open('index.php','_self')</script>";
+    }
+  }
+  $stmt->close();
+}
 		 exit();
 
-    }
 }
 ?>
